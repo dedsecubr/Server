6996399724910f4368eb8d2419493ac7d5e4c3e4
Ded Security <47339140+dedsecubr@users.noreply.github.com>
2020-12-04 22:27:05 -0300

    server

---
+++ deps.ts
@@ -0,0 +1,4 @@
+import * as path from "https://cdn.deno.land/std/versions/0.75.0/raw/path/mod.ts";
+import * as http from "https://cdn.deno.land/std/versions/0.75.0/raw/http/server.ts";
+import { red, yellow, cyan, green } from "https://cdn.deno.land/std/versions/0.75.0/raw/fmt/colors.ts";
+export { path, http, red, yellow, cyan, green };
---
+++ test.ts
@@ -0,0 +1,30 @@
+import { assert, assertEquals, assertThrows} from "https://deno.land/std@0.75.0/testing/asserts.ts"; 
+import { Request, simplePathMatcher } from "./mod.ts";
+
+Deno.test({ name: "testParse_url", fn() { 
+  const req = new Request({
+        url: "/files-tmb/1234/abc.png?key=val"
+      });
+      assertEquals(req.path, "/files-tmb/1234/abc.png");
+      assertEquals(req.query.key, "val");
+}});
+
+Deno.test({name: "testSimplePathMatcher", fn() { 
+    assert(!!simplePathMatcher("/")("/"));
+    assert(!!simplePathMatcher("/foo")("/foo"));
+    assert(!!simplePathMatcher("/foo/")("/foo/"));
+    assert(!!simplePathMatcher("/foo/1")("/foo/1"));
+    assertEquals(simplePathMatcher("/foo")("/"), null);
+    assertEquals(simplePathMatcher("/foo")("/fooo"), null);
+    assertEquals(simplePathMatcher("/foo")("/foo/"), null);
+    assertEquals(simplePathMatcher("/{a}")("/foo").a, "foo");
+    assertEquals(simplePathMatcher("/{a}/foo/{xxx}")("/34/foo/1").a, "34");
+    assertEquals(simplePathMatcher("/{a}/foo/{xxx}")("/34/foo/1").xxx, "1");
+    for (let v of ["//", "/{}", "/{x}/{x}"]) {
+      assertThrows(() => {
+        simplePathMatcher(v);
+      });
+    }
+}});
+
+//await Deno.runTests();
