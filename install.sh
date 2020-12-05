c220d402438d514ff91e6f2e464515373f632de8
Ded Security <47339140+dedsecubr@users.noreply.github.com>
2020-12-05 18:55:35 -0300

    server

---
+++ install.sh
@@ -0,0 +1,2 @@
+# Deno for linux
+curl -fsSL https://deno.land/x/install/install.sh | sh
--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { serve } from "https://raw.githubusercontent.com/dedsecubr/deno/master/std/http/server.ts";
 
-const s = serve({ port: 8000 });
+const s = serve({ hostname: "0.0.0.0", port: 8000 });
 
 console.log('%cHTTP '+'%cserver listening on http://localhost:8000','color:green;','color:white;');
 
 for await (const req of s) {
 
-  const html_file = await Deno.readFile('./index.html');
+  const html_file = await Deno.readFile('./public/index.html');
   const decoder = new TextDecoder()
   req.respond({ body: decoder.decode(html_file) });
 }
