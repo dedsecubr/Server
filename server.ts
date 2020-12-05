cba20a8800a1b51f98aaf1316bd48d2860da5923
Ded Security <47339140+dedsecubr@users.noreply.github.com>
2020-12-05 17:42:30 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std/http/server.ts";
 
 const s = serve({ port: 8000 });
 
-console.log('HTTP server listening on http://localhost:8000');
+console.log('%cHTTP '+'%cserver listening on http://localhost:8000','color:green;','color:white;');
 
 for await (const req of s) {
 
